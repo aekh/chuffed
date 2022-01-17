1fb7e03a2d8f62ed1116c298ea1b8b70f1390dc9
Alexander Ek <djalekzse@gmail.com>
2022-01-17 15:28:42 +1100

    test

--- chuffed/vars/int-var.cpp
+++ chuffed/vars/int-var.cpp
@@ -31,7 +31,7 @@ IntVar::IntVar(int _min, int _max) :
   , activity(0)
   , in_queue(false)
 {
-  printf("%% id: %d :: %d <= %d <= %d <= %d\n", var_id, min_limit, min, max, max_limit);
+  printf("%% id: %d :: %d <= %d <= %d <= %d\n", var_id, min_limit, _min, _max, max_limit);
   assert(min_limit <= min && min <= max && max <= max_limit);
 	engine.vars.push(this);
 	changes = EVENT_C | EVENT_L | EVENT_U;
